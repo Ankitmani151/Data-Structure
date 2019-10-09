ea5b5212bf7036ce3ae1e3bb1562932dd157b80e
Ankitmani151 <55287567+Ankitmani151@users.noreply.github.com>
2019-10-09 19:18:07 +0530

    Update Stack_Queue_Deque.py

--- Stack_Queue_Deque.py
+++ Stack_Queue_Deque.py
@@ -1,3 +1,5 @@
+#Implementing a Stack
+
 class Stack(object):
     
     def __init__(self):
@@ -17,4 +19,55 @@ class Stack(object):
     
     def size(self):
         return len(self.items)
+    
+##################################################################################################################
+    
+#Implementing a Queue
+
+class Queue(object):
+    
+    def __init__(self):
+        self.items=[]
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def enqueue(self,item):
+        self.items.insert(0,item)
+        
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+    
+#################################################################################################################
+
+#Implementing a Deque
+
+class Deque(object):
+    
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items==[]
+    
+    def addFront(self,item):
+        self.items.append(item)
+        
+    def addRear (self,item):
+        self.items.insert(0,item)
+        
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        return len(self.items)
+    
+
+    
     
