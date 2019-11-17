29e377c23aa36c8cd0794e9048315525af605963
Ankitmani151 <55287567+Ankitmani151@users.noreply.github.com>
2019-11-17 10:31:51 +0530

    Selectionsort.py

---
+++ selection.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    for fillslot in range(len(arr)-1,0,-1):
+        positionOfMax=0
+        for location in range(1,fillslot+1):
+            # Set maximum's location
+            if arr[location]>arr[positionOfMax]:
+                positionOfMax = location
+
+        temp = arr[fillslot]
+        arr[fillslot] = arr[positionOfMax]
+        arr[positionOfMax] = temp
+     
