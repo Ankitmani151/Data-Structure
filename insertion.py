b8762b20c2533072cf7ee787d74c5ebc204b6deb
Ankitmani151 <55287567+Ankitmani151@users.noreply.github.com>
2019-11-18 19:26:57 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -1,16 +1,8 @@
-def insertion_sort(arr):
-    
-    # For every index in array
-    for i in range(1,len(arr)):
+for i in range(1,len(arr)):
+    curnum=arr[i]
+    position=i
+    while (position>0 and arr[position-1]>curnum):
+        arr[position]=arr[position-1]
+        position=position-1
         
-        # Set current values and position
-        currentvalue = arr[i]
-        position = i
-        
-        # Sorted Sublist
-        while position>0 and arr[position-1]>currentvalue:
-            
-            arr[position]=arr[position-1]
-            position = position-1
-
-        arr[position]=currentvalue
+    arr[position]=curnum
